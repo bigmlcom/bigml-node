2d2c7550189f2049de8789071afc0f5f1c141dd6
Mercè Martín <merce@bigml.com>
2013-06-02 16:51:44 +0200

    Full source tests

--- test/BigMLSource-test.js
+++ test/BigMLSource-test.js
@@ -7,19 +7,42 @@ describe('Manage source objects', function(){
   describe('#create(path, callback)', function(){
     it('should create a source from a file', function(done){
         source.create(path, undefined, function (error, data) {
-        assert.equal(data.code, constants.HTTP_CREATED);
-        sourceId = data.resource;
-        done();
+            assert.equal(data.code, constants.HTTP_CREATED);
+            sourceId = data.resource;
+            done();
       });
     })
   })
-  describe('#get(sourceId, finished, callback)', function(){
+  describe('#get(sourceId, finished, query, callback)', function(){
     it('should retrieve a finished source', function(done){
         source.get(sourceId, true, function (error, data) {
-        if (data.object.status.code === constants.FINISHED) {
-          assert.ok(true);
-          done();
-        }
+            if (data.object.status.code === constants.FINISHED) {
+              assert.ok(true);
+              done();
+            }
+      });
+    })
+  })
+  describe('#update(source, args, callback)', function(){
+    it('should update properties in the source', function(done){
+        var newName = 'my new name';
+        source.update(sourceId, {name: newName}, function (error, data) {
+            assert.equal(data.code, constants.HTTP_ACCEPTED);
+            source.get(sourceId, true, function (errorcb, datacb) {
+                if (datacb.object.status.code === constants.FINISHED &&
+                    datacb.object.name == newName) {
+                  assert.ok(true);
+                  done();
+                }
+            });
+      });
+    })
+  })
+  describe('#delete(source, args, callback)', function(){
+    it('should delete the remote source', function(done){
+        source.delete(sourceId, function (error, data) {
+            assert.equal(error, null);
+            done();
       });
     })
   })
