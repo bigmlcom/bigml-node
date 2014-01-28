612cdb8fbca247577185c07ce9bcca762b454e21
Mercè Martín <merce@bigml.com>
2014-01-28 01:30:43 +0100

    Adding test file and bath prediction missing file

---
+++ test/BatchPrediction-test.js
@@ -0,0 +1,131 @@
+var assert = require('assert'),
+  bigml = require('../index'),
+  fs = require('fs');
+
+describe('Manage batch prediction objects', function () {
+  var sourceId, source = new bigml.Source(), path = './data/iris.csv',
+    datasetId, dataset = new bigml.Dataset(),
+    modelId, model = new bigml.Model(),
+    batchPredictionId, batchPrediction = new bigml.BatchPrediction(),
+    trainingDatasetId, testDatasetId,
+    tmpFileName = '/tmp/testBatchPrediction.csv';
+
+  before(function (done) {
+    var seed = 'BigML, Machine Learning made simple',
+      sampleRate = 0.8;
+    source.create(path, undefined, function (error, data) {
+      assert.equal(data.code, bigml.constants.HTTP_CREATED);
+      sourceId = data.resource;
+      dataset.create(sourceId, undefined, function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_CREATED);
+        datasetId = data.resource;
+        dataset.create(datasetId, {seed: seed, sample_rate: sampleRate},
+                       function (error, data) {
+            assert.equal(data.code, bigml.constants.HTTP_CREATED);
+            trainingDatasetId = data.resource;
+            dataset.create(datasetId, {seed: seed,
+                                       sample_rate: sampleRate,
+                                       out_of_bag: true},
+                           function (error, data) {
+                assert.equal(data.code, bigml.constants.HTTP_CREATED);
+                testDatasetId = data.resource;
+                model.create(trainingDatasetId, undefined, function (error, data) {
+                  assert.equal(data.code, bigml.constants.HTTP_CREATED);
+                  modelId = data.resource;
+                  done();
+                });
+              });
+          });
+      });
+    });
+  });
+
+  describe('#create(model, testDatasetId, args, callback)', function () {
+    it('should create a batch prediction for a model and a dataset', function (done) {
+      batchPrediction.create(modelId, testDatasetId, undefined,
+                             function (error, data) {
+          assert.equal(data.code, bigml.constants.HTTP_CREATED);
+          batchPredictionId = data.resource;
+          done();
+        });
+    });
+  });
+  describe('#get(batchPrediction, finished, query, callback)', function () {
+    it('should retrieve a finished batch prediction', function (done) {
+      batchPrediction.get(batchPredictionId, true, function (error, data) {
+        if (data.object.status.code === bigml.constants.FINISHED) {
+          assert.ok(true);
+          done();
+        }
+      });
+    });
+  });
+  describe('#download(batchPrediction, filename, callback)', function () {
+    it('should download the batch prediction output file', function (done) {
+      batchPrediction.download(batchPredictionId, tmpFileName, function (cbFilename) {
+        fs.exists(cbFilename, function (exists) {
+          assert.ok(exists);
+          try {
+            fs.unlink(cbFilename);
+          } catch (err) {}
+          done();
+        });    
+      });
+    });
+  });
+  describe('#update(batchPrediction, args, callback)', function () {
+    it('should update properties in the batch prediction', function (done) {
+      var newName = 'my new name';
+      batchPrediction.update(batchPredictionId, {name: newName}, function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_ACCEPTED);
+        batchPrediction.get(batchPredictionId, true, function (errorcb, datacb) {
+          if (datacb.object.status.code === bigml.constants.FINISHED &&
+              datacb.object.name === newName) {
+            assert.ok(true);
+            done();
+          }
+        });
+      });
+    });
+  });
+  describe('#delete(batchPrediction, callback)', function () {
+    it('should delete the remote batch prediction', function (done) {
+      batchPrediction.delete(batchPredictionId, function (error, data) {
+        assert.equal(error, null);
+        done();
+      });
+    });
+  });
+
+
+  after(function (done) {
+    source.delete(sourceId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    dataset.delete(datasetId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    dataset.delete(trainingDatasetId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    dataset.delete(testDatasetId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    model.delete(modelId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+});
