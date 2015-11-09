fb6a4ed92a03398fac8c7674cc6134829a5c602b
Mercè Martín Prats <merce@bigml.com>
2015-11-09 21:02:44 +0100

    Changing test for precision changes

--- test/LocalLogisticRegression-test.js
+++ test/LocalLogisticRegression-test.js
@@ -8,7 +8,7 @@ describe('Manage local logistic regression objects', function () {
     logisticId, logistic = new bigml.LogisticRegression(),
     logisticResource, logisticFinishedResource,
     localLogisticRegression,
-    prediction1 = {"prediction":"Iris-virginica","probability":0.5480947525526224,"distribution":[{"category":"Iris-virginica","probability":0.5480947525526224},{"category":"Iris-versicolor","probability":0.2443772679765604},{"category":"Iris-setosa","probability":0.20752797947081716}]},
+    prediction1 = {"prediction":"Iris-virginica","probability":0.5480947525526229,"distribution":[{"category":"Iris-virginica","probability":0.5480947525526229},{"category":"Iris-versicolor","probability":0.24437726797656012},{"category":"Iris-setosa","probability":0.20752797947081705}]},
     inputData1 = {'petal length': 1, 'sepal length': 1,
                   'petal width': 1, 'sepal width': 1};
 
