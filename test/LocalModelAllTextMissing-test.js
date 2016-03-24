164463b5d01592a9328895daaf203b72d32a9031
Mercè Martín Prats <merce@bigml.com>
2016-03-24 01:20:56 +0100

    Fixing bug in local model predictions for missing text or items

---
+++ test/LocalModelAllTextMissing-test.js
@@ -0,0 +1,84 @@
+var assert = require('assert'),
+  bigml = require('../index');
+
+describe('Manage local model objects', function () {
+  var sourceId, source = new bigml.Source(), path = './data/text_missing.csv',
+    datasetId, dataset = new bigml.Dataset(),
+    missingSplits = {"missing_splits": false},
+    modelId, model = new bigml.Model(), modelResource, modelFinishedResource,
+    localModel, firstPredictionConfidence, secondPredictionConfidence;
+
+  before(function (done) {
+    var tokenMode = {'fields': {'000001': {'term_analysis': {'token_mode': 'all'}}, '000000': {'term_analysis': {'token_mode': 'all'}}}},
+      textField = {'fields': {'000001': {'optype': 'text'}, '000000': {'optype': 'text'}}};
+    source.create(path, undefined, function (error, data) {
+      assert.equal(data.code, bigml.constants.HTTP_CREATED);
+      sourceId = data.resource;
+      source.get(sourceId, true, function (error, data) {
+        source.update(sourceId, textField, function (error, data) {
+          dataset.create(sourceId, tokenMode, function (error, data) {
+            assert.equal(data.code, bigml.constants.HTTP_CREATED);
+            datasetId = data.resource;
+            model.create(datasetId, missingSplits, function (error, data) {
+              assert.equal(data.code, bigml.constants.HTTP_CREATED);
+              modelId = data.resource;
+              modelResource = data;
+              model.get(modelResource, true, 'only_model=true', function (error, data) {
+                modelFinishedResource = data;
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe('LocalModel(modelId)', function () {
+    it('should create a localModel from a model Id', function (done) {
+      localModel = new bigml.LocalModel(modelId);
+      if (localModel.ready) {
+        assert.ok(true);
+        done();
+      } else {
+        localModel.on('ready', function () {assert.ok(true);
+          done();
+          });
+      }
+    });
+  });
+  describe('#predict(inputData, callback)', function () {
+    it('should predict asynchronously from input data', function (done) {
+      localModel.predict({}, function (error, data) {
+        assert.equal(data.prediction, 'paperwork');
+        firstPredictionConfidence = data.confidence;
+        done();
+      });
+    });
+  });
+  describe('#predict(inputData)', function () {
+    it('should predict synchronously from input data', function () {
+      var prediction = localModel.predict({});
+      assert.equal(prediction.prediction, 'paperwork');
+      secondPredictionConfidence = prediction.confidence;
+    });
+  });
+  after(function (done) {
+    source.delete(sourceId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    dataset.delete(datasetId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    model.delete(modelId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+});
