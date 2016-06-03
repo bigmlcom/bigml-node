8618586813bc667c5fc2a57fde00f87d5c85dfe9
Mercè Martín Prats <merce@bigml.com>
2016-06-03 20:04:55 +0200

    Adapting LocalLogisticRegressionn to structure changes in backend

--- test/LocalLogisticRegressionCodings-test.js
+++ test/LocalLogisticRegressionCodings-test.js
@@ -2,6 +2,23 @@ var assert = require('assert'),
   bigml = require('../index'),
   constants = require('../lib/constants');
 
+function formatAPIPrediction(data) {
+  var index, probabilities = data.object.probabilities, probability;
+  for (index = 0; index < probabilities.length; index++) {
+    if (probabilities[index][0] == data.object.output) {
+      probability = probabilities[index][1];
+      break;
+    }
+  }
+  return {prediction: data.object.output,
+          probability: probability};
+}
+
+function truncate(number, decimals) {
+  return Math.round(number * Math.pow(10, decimals)) / Math.pow(10.0,
+                                                                decimals);
+}
+
 describe('Manage local logistic regression objects', function () {
   var sourceId, source = new bigml.Source(), path = './data/iris.csv',
     datasetId, dataset = new bigml.Dataset(),
@@ -11,14 +28,14 @@ describe('Manage local logistic regression objects', function () {
     localLogisticRegression, prediction = new bigml.Prediction(),
     objective1 = "000000",
     fieldCodings1 = {species: {contrast: [[1, 2, -1, -2]]}}
-    prediction1 = {prediction: '5.5', probability: 0.04292825759854652},
+    prediction1 = {prediction: '5.5', probability: 0.04293},
     inputData1 = {'species': 'Iris-setosa'},
     objective2 = "000000",
     fieldCodings2 = {species: {other: [[1, 2, -1, -2]]}}
     prediction2 = prediction1,
     objective3 = "000000",
     fieldCodings3 = {species: {dummy: "Iris-setosa"}},
-    prediction3 = {prediction: '6.9', probability: 0.02857142857142857};
+    prediction3 = {prediction: '5.0', probability: 0.02857};
 
   before(function (done) {
     source.create(path, undefined, function (error, data) {
@@ -74,8 +91,15 @@ describe('Manage local logistic regression objects', function () {
     it('should predict asynchronously from input data', function (done) {
       localLogisticRegression.predict(inputData1, function (error, data) {
         delete data["distribution"];
+        data.probability = truncate(data.probability, 5);
         assert.equal(JSON.stringify(data), JSON.stringify(prediction1));
-        done();
+        prediction.create(logisticId1, inputData1, function (error, data) {
+            apiPrediction = formatAPIPrediction(data);
+              assert.equal(JSON.stringify(apiPrediction),
+                           JSON.stringify(prediction1));
+            prediction.delete(data.resource);
+            done();
+          });
       });
     });
   });
@@ -99,8 +123,15 @@ describe('Manage local logistic regression objects', function () {
     it('should predict asynchronously from input data', function (done) {
       localLogisticRegression.predict(inputData1, function (error, data) {
         delete data["distribution"];
+        data.probability = truncate(data.probability, 5);
         assert.equal(JSON.stringify(data), JSON.stringify(prediction2));
-        done();
+        prediction.create(logisticId2, inputData1, function (error, data) {
+            apiPrediction = formatAPIPrediction(data);
+              assert.equal(JSON.stringify(apiPrediction),
+                           JSON.stringify(prediction2));
+            prediction.delete(data.resource);
+            done();
+          });
       });
     });
   });
@@ -124,8 +155,16 @@ describe('Manage local logistic regression objects', function () {
     it('should predict asynchronously from input data', function (done) {
       localLogisticRegression.predict(inputData1, function (error, data) {
         delete data["distribution"];
+        data.probability = truncate(data.probability, 5);
         assert.equal(JSON.stringify(data), JSON.stringify(prediction3));
-        done();
+        prediction.create(logisticId3, inputData1, function (error, data) {
+            console.log(data.object.probabilities);
+            apiPrediction = formatAPIPrediction(data);
+              assert.equal(JSON.stringify(apiPrediction),
+                           JSON.stringify(prediction3));
+            prediction.delete(data.resource);
+            done();
+          });
       });
     });
   });
