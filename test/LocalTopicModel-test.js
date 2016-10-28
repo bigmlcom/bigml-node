67d6b62f2a1b8cf6e8c7d2ae8f0746d1c0854f36
Charles Parker <parker@bigml.com>
2016-10-28 10:10:46 -0500

    Handle quotes correctly in LocalTopicModel

--- test/LocalTopicModel-test.js
+++ test/LocalTopicModel-test.js
@@ -17,7 +17,7 @@ describe('Local Topic Model regression', function () {
                                  [0, 1, 2, 0],
                                  [1, 2, 0, 0],
                                  [0, 0, 2, 0]],
-      "termset": ["cycling", "playing", "tacos", "unanimous court"],
+      "termset": ["cycling", "playing", "shouldn't", "unanimous court"],
       "options": {},
       "topics": [{"name": "Topic 1",
                   "id": "000000",
@@ -51,8 +51,8 @@ describe('Local Topic Model regression', function () {
   };
 
   var test_text = "unanimous court UNANIMOUS COURT\n\n " +
-        "play the plays PLAYing TACO CYCLE        " +
-        "cycling tacos unanimous or court";
+        "play the plays PLAYing SHOULDN'T CYCLE        " +
+        "cycling shouldn't unanimous or court";
 
   var expected_distribution = [
     { name: 'Topic 1', probability: 0.1647366 },
