a73984b186f53e6b17734f6a7c09bf025dca869a
Mercè Martín Prats <merce@bigml.com>
2017-02-07 23:34:55 +0100

    New bindings version and copyright notices in test files

--- test/LocalModelFile-test.js
+++ test/LocalModelFile-test.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017 BigML
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 var assert = require('assert'),
   bigml = require('../index'),
   constants = require('../lib/constants');
@@ -38,7 +54,7 @@ describe('Manage local model objects', function () {
     });
   });
   describe('#predict(inputData, callback)', function () {
-    it('should predict asynchronously from input data keyed by field id', 
+    it('should predict asynchronously from input data keyed by field id',
        function (done) {
       localModel.predict({'000003': 0.5}, function (error, data) {
         assert.equal(data.prediction, 'Iris-setosa');
