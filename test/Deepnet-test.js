e7953c91557a3d04abfa694645f916d30d1e8422
Mercè Martín Prats <merce@bigml.com>
2017-09-19 16:58:22 +0200

    First version of deepnets

---
+++ test/Deepnet-test.js
@@ -0,0 +1,117 @@
+/**
+ * Copyright 2017 BigML
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+var assert = require('assert'),
+  bigml = require('../index'),
+  path = require('path');
+var scriptName = path.basename(__filename);
+
+describe(scriptName + ': Manage deepnet objects', function () {
+  var sourceId, source = new bigml.Source(), path = './data/iris.csv',
+    datasetId, dataset = new bigml.Dataset(), datasetId2,
+    dataset2 = new bigml.Dataset(),
+    deepnetId, deepnetId2, deepnet = new bigml.Deepnet();
+
+  before(function (done) {
+    source.create(path, undefined, function (error, data) {
+      assert.equal(data.code, bigml.constants.HTTP_CREATED);
+      sourceId = data.resource;
+      dataset.create(sourceId, undefined, function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_CREATED);
+        datasetId = data.resource;
+        dataset2.create(sourceId, undefined, function (error, data) {
+          assert.equal(data.code, bigml.constants.HTTP_CREATED);
+          datasetId2 = data.resource;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#create(dataset, args, callback)', function () {
+    it('should create a deepnet from a dataset', function (done) {
+      deepnet.create(datasetId, undefined, function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_CREATED);
+        deepnetId = data.resource;
+        done();
+      });
+    });
+  });
+  describe('#get(deepnet, finished, query, callback)', function () {
+    it('should retrieve a finished deepnet', function (done) {
+      deepnet.get(deepnetId, true, function (error, data) {
+        if (data.object.status.code === bigml.constants.FINISHED) {
+          assert.ok(true);
+          done();
+        }
+      });
+    });
+  });
+  describe('#update(deepnet, args, callback)', function () {
+    it('should update properties in the deepnet', function (done) {
+      var newName = 'my new name';
+      deepnet.update(deepnetId, {name: newName}, function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_ACCEPTED);
+        deepnet.get(deepnetId, true, function (errorcb, datacb) {
+          if (datacb.object.status.code === bigml.constants.FINISHED &&
+              datacb.object.name === newName) {
+            assert.ok(true);
+            done();
+          }
+        });
+      });
+    });
+  });
+  describe('#create([dataset], args, callback)', function () {
+    it('should create a deepnet from a list of datasets ',
+      function (done) {
+        deepnet.create([datasetId, datasetId2], undefined,
+                    function (error, data) {
+        assert.equal(data.code, bigml.constants.HTTP_CREATED);
+        deepnetId2 = data.resource;
+        done();
+      });
+    });
+  });
+
+  describe('#delete(deepnet, callback)', function () {
+    it('should delete the remote deepnet', function (done) {
+      deepnet.delete(deepnetId, function (error, data) {
+        assert.equal(error, null);
+        deepnet.delete(deepnetId2, function (error, data) {
+          assert.equal(error, null);
+          done();
+        });
+      });
+    });
+  });
+
+  after(function (done) {
+    source.delete(sourceId, function (error, data) {
+      assert.equal(error, null);
+      done();
+    });
+  });
+  after(function (done) {
+    dataset.delete(datasetId, function (error, data) {
+      assert.equal(error, null);
+      dataset2.delete(datasetId2, function (error, data) {
+        assert.equal(error, null);
+        done();
+      });
+    });
+  });
+});
