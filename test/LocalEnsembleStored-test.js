55bd3a908a16f5c0332c434cc095ed8fb67b0fae
Mercè Martín Prats <merce@bigml.com>
2018-02-21 11:50:06 +0100

    Adding test for local predictions fix

---
+++ test/LocalEnsembleStored-test.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2017-2018 BigML
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+var assert = require('assert'),
+  bigml = require('../index'),
+  path = require('path');
+var scriptName = path.basename(__filename);
+
+describe(scriptName + ': Manage local ensemble objects', function () {
+  var inputData = {"000002": 3, "000005": 3407, "000007": 1958,
+                   "00000b": 601875, "00000c": 44.63, "00000d": -64338.6},
+    modelsList = ["model/5a8c5fec8a318f490a00400c",
+                  "model/5a8c5fec8a318f490a00400e",
+                  "model/5a8c5feb8a318f490a004008"],
+    index,
+    reference = '{"prediction":97330,"confidence":0.368635}',
+    localEnsemble, len, missingStrategy = 1;
+
+  describe('LocalEnsemble(ensemble)', function () {
+    it('should create a localEnsemble from a list of stored models', function (done) {
+      localEnsemble = new bigml.LocalEnsemble(
+        modelsList,
+        new bigml.BigML(undefined, undefined, {storage: "data/storage"}));
+      if (localEnsemble.ready) {
+        assert.ok(true);
+        done();
+      } else {
+        localEnsemble.on('ready', function () {assert.ok(true);
+          done();
+          });
+      }
+    });
+  });
+  describe('#predict(inputData, method, callback)', function () {
+    it('should predict asynchronously from input data', function (done) {
+      localEnsemble.predict(inputData, undefined, {missingStrategy: missingStrategy},
+        function (error, data) {
+          assert.equal(JSON.stringify(data), reference);
+          done();
+      });
+    });
+  });
+  describe('#predict(inputData, method)', function () {
+    it('should predict synchronously from input data', function () {
+      var result = localEnsemble.predict(inputData, undefined,
+        {missingStrategy: missingStrategy});
+      assert.equal(JSON.stringify(result), reference);
+    });
+  });
+  describe('#predict(inputData, method)', function () {
+    it('should predict synchronously from input data', function () {
+      var result = localEnsemble.predict(inputData, undefined, {missingStrategy: missingStrategy});
+      assert.equal(JSON.stringify(result), reference);
+    });
+  });
+});
